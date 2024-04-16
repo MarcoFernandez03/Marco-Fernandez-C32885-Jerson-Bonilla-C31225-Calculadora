41a5e8b5d249a96102674c9d1fd921c821cfb856
marco.fernandezchacon <marco.fernandezchacon@ucr.ac.cr>
2024-04-16 16:07:29 -0600

    actualizada clase controladora con implementación de tokenizer, documentación de tokenizer

--- src/tokenizer.cc
+++ src/tokenizer.cc
@@ -3,12 +3,14 @@
 #include <tokenizer.hh>
 using namespace std;
 
+//Function that gets an string and classifies its characters as tokens 
 int tokenizer::tokenize(string userExpression) {
   for (int i = 0; i < userExpression.length(); i++) {
     if (isdigit(userExpression[i])) {
       // PlaceHolder cambiar por lógica cuando Token esté implementada
       cout << "Estoy leyendo un numero " << userExpression[i] << endl;
     } else {
+      //Switch used to dictate an state based on the char read.
       switch (userExpression[i]) {
       case '(':
         cout << "Estoy leyendo el parentesis " << userExpression[i] << endl;
