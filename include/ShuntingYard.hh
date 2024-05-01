49497b08cb85db7b26554433c403d9f824ba470f
marco.fernandezchacon <marco.fernandezchacon@ucr.ac.cr>
2024-05-01 13:36:24 -0600

    ShuntingYard: Eliminado isValid, agregado distintos errores con parentesis, nueva forma de recibir precedencia

--- include/ShuntingYard.hh
+++ include/ShuntingYard.hh
@@ -4,15 +4,14 @@
 using namespace std;
 #ifndef TOKENIZER_HH
 #define TOKENIZER_HH
-class ShuntingYard
-{
-    public:
-    stack<Token> parse(stack<Token> tokenizedExpression);
-    private:
-    bool isValid(stack<Token> expressionToCheck);
-    stack<Token> tokenStack;
-    stack<Token> operatorStack;
-    map<char, int> precedence;
+class ShuntingYard {
+public:
+  stack<Token> parse(stack<Token> tokenizedExpression);
 
+private:
+  bool isValid(stack<Token> expressionToCheck);
+  stack<Token> tokenStack;
+  stack<Token> operatorStack;
+  int GetPrecedence(char token);
 };
 #endif
